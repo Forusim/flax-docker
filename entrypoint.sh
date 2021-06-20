8d567cafab41caf4e9cbb480907ab976871fb72f
Forusim <forusim@web.de>
2021-06-21 01:04:41 +0200

    Do not start before keys added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-DBDIR=/root/.chia
+DBDIR=/root/.chia/mainnet/db
 
 cd /chia-blockchain
 
 . ./activate
-
-chia init
 
 if [[ ! -d $DBDIR ]]; then
     if [[ ${keys} == "generate" ]]; then
@@ -25,38 +23,30 @@ if [[ ! -d $DBDIR ]]; then
     else
       chia keys add -f ${keys}
     fi
-fi
-
-for p in ${plots_dir//:/ }; do
-    mkdir -p ${p}
-    if [[ ! "$(ls -A $p)" ]]; then
-        echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
-    fi
-    chia plots add -d ${p}
-done
-
-sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
-
-if [[ ${farmer} == 'true' ]]; then
-  chia start farmer-only
-elif [[ ${harvester} == 'true' ]]; then
-  if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
-    echo "A farmer peer address, port, and ca path are required."
-    exit
-  else
-    chia configure --set-farmer-peer ${farmer_address}:${farmer_port}
-    chia start harvester
-  fi
 else
-  chia start farmer
-fi
+    for p in ${plots_dir//:/ }; do
+        mkdir -p ${p}
+        if [[ ! "$(ls -A $p)" ]]; then
+            echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
+        fi
+        chia plots add -d ${p}
+    done
 
-if [[ ${testnet} == "true" ]]; then
-  if [[ -z $full_node_port || $full_node_port == "null" ]]; then
-    chia configure --set-fullnode-port 58444
-  else
-    chia configure --set-fullnode-port ${var.full_node_port}
-  fi
+    sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
+
+    if [[ ${farmer} == 'true' ]]; then
+      chia start farmer-only
+    elif [[ ${harvester} == 'true' ]]; then
+      if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
+        echo "A farmer peer address, port, and ca path are required."
+        exit
+      else
+        chia configure --set-farmer-peer ${farmer_address}:${farmer_port}
+        chia start harvester
+      fi
+    else
+      chia start farmer
+    fi
 fi
 
 while true; do sleep 30; done;
