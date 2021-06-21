5aed727194b30c6a3f1f0c6b89a0ecee5674dae4
Forusim <forusim@web.de>
2021-06-21 23:25:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-DBDIR=/root/.chia/mainnet/db
+DBDIR=/root/.flax/mainnet/db
 
-cd /chia-blockchain
+cd /flax-blockchain
 
 . ./activate
 
 if [[ ! -d $DBDIR ]]; then
     if [[ ${keys} == "generate" ]]; then
       echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
-      chia keys generate
+      flax keys generate
     elif [[ ${keys} == "copy" ]]; then
       if [[ -z ${ca} ]]; then
         echo "A path to a copy of the farmer peer's ssl/ca required."
         exit
       else
-      chia init -c ${ca}
+      flax init -c ${ca}
       fi
     elif [[ ${keys} == "type" ]]; then
-      echo "Call from docker shell: chia keys add"
+      echo "Call from docker shell: flax keys add"
       echo "Restart the container after mnemonic input"
     else
-      chia keys add -f ${keys}
+      flax keys add -f ${keys}
     fi
 else
     for p in ${plots_dir//:/ }; do
@@ -29,23 +29,23 @@ else
         if [[ ! "$(ls -A $p)" ]]; then
             echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
         fi
-        chia plots add -d ${p}
+        flax plots add -d ${p}
     done
 
-    sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
+    sed -i 's/localhost/127.0.0.1/g' ~/.flax/mainnet/config/config.yaml
 
     if [[ ${farmer} == 'true' ]]; then
-      chia start farmer-only
+      flax start farmer-only
     elif [[ ${harvester} == 'true' ]]; then
       if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
         echo "A farmer peer address, port, and ca path are required."
         exit
       else
-        chia configure --set-farmer-peer ${farmer_address}:${farmer_port}
-        chia start harvester
+        flax configure --set-farmer-peer ${farmer_address}:${farmer_port}
+        flax start harvester
       fi
     else
-      chia start farmer
+      flax start farmer
     fi
 fi
 
